e2315f0a8699169813043659c103b6efe5297bcb
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-02-21 00:16:54 -0300

    organização das urls

--- functions/bitly.py
+++ functions/bitly.py
@@ -10,12 +10,54 @@ def bitly():
     api_key_bitly = [f"{os.environ.get('API_KEY_BITLY')}"]
     shortener = bitlyshortener.Shortener(tokens=api_key_bitly)
 
-    numbers_urls = int(input('How many urls do you want to shorten? '))
-    long_urls = []
-    for i in range(numbers_urls):
-        url = input('Add url: ')
-        long_urls.append(url)
+    numbers_urls = str(input('How many urls do you want to shorten? '))
 
-    long_urls = shortener.shorten_urls(long_urls)
+    if numbers_urls == 'exit':
+        exit()
+    elif numbers_urls.isdigit():
+        numbers_urls = int(numbers_urls)
 
-    print(f'Shortened url: {long_urls}')
+        long_urls = []
+        name_urls = []
+        id_urls = []
+
+        urls_info = {}
+
+        for i in range(numbers_urls):
+            print(i + 1, 'url')
+            name = input('Add a url name: ')
+            url = input('Add url: ')
+
+            long_urls = shortener.shorten_urls([url])
+
+            urls_info[i] = [{'name': name, 'url': long_urls[0]}]
+
+            long_urls.clear()
+
+            # id_urls.append(i)
+            # name_urls.append(name)
+            # long_urls.append(url)
+
+            print()
+
+        # long_urls = shortener.shorten_urls(long_urls)
+
+        for id_dict, id_content in urls_info.items():
+            for infos in id_content:
+                for name_url, url_short in infos.items():
+                    print(name_url, url_short)
+                    # [] Mostrar o valor de uma forma mais bonitinha hahaha
+
+            # show_id_url = id_urls[i] + 1
+            # show_name_url = name_urls[i]
+            # show_long_url = long_urls[i]
+
+            # print(f'{show_id_url}) {show_name_url}: {show_long_url}')
+
+
+# for x, y in thisdict.items():
+#     print(x, y)
+#     if x == 'year':
+#         for i in y:
+#             for o, p in i.items():
+#                 print(p)
--- main.py
+++ main.py
@@ -10,12 +10,16 @@ print(f'Choose a shortener...\n',
       f'{c["FontRed"]}[1]{c["FontPurple"]} {c["StyleBold"]}CUTTLY {c["ResetAll"]}\n',
       f'{c["FontRed"]}[2]{c["FontPurple"]} {c["StyleBold"]}BITLY {c["ResetAll"]}(more than one URL)\n',
       f'{c["FontRed"]}[3]{c["FontPurple"]} {c["StyleBold"]}NONE {c["ResetAll"]}\n',
+      f'{c["FontRed"]}[exit]{c["FontPurple"]} {c["StyleBold"]}TO EXIT {c["ResetAll"]}\n',
       )
 
 chooseShortener = str(input(
     f'{c["FontRed"]}{c["StyleBold"]}--->>>{c["ResetAll"]} ')).strip()
 
-if chooseShortener.isdigit():
+
+if chooseShortener == 'exit':
+    exit()
+elif chooseShortener.isdigit():
     chooseShortenerInt = int(chooseShortener)
 
     if chooseShortenerInt == 1:
@@ -31,6 +35,5 @@ if chooseShortener.isdigit():
     else:
         print(
             f'Option "{c["FontYellow"]}{chooseShortenerInt}{c["ResetAll"]}" is not defined.')
-
 else:
     print(f'{c["FontRed"]}ERROR!{c["ResetAll"]} \"{c["FontYellow"]} {chooseShortener} {c["ResetAll"]}\" is not defined.')
